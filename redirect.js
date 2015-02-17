cbca5a2bb1a1893c462f1f8650d498cbb623f1b2
Benjamin Conant <conantbenjamin@gmail.com>
2015-02-17 15:55:14 -0500

    updated manifest and script for inbox

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
 function goToGmail() {
-	chrome.tabs.create({ url: "http://mail.google.com" });
+	chrome.tabs.create({ url: "http://inbox.google.com" });
 }
 chrome.browserAction.onClicked.addListener(goToGmail);
